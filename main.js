d60b5f54b81f125505f6746f4adb4a4d69417100
Richard Gautereaux <Richieg72@gmail.com>
2015-11-04 09:21:27 -0800

    Fixed a little Geo-Location

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ var apiUrl = 'http://api.wunderground.com/api/1d5cbad3ecc470d6/'
 $(document).ready(init);
 
 
-
+var geoUrl;
 
 function init() {
   $('#getWeather').on('click', getWeather)
-  var geoUrl = apiUrl + '/geolookup/q/autoip.json'
+  geoUrl = apiUrl + '/geolookup/q/autoip.json'
   $.get(geoUrl)
   .done(function(data){
     console.log(data)
+    locData = data
+    var city = data.location.city
+    var state = data.location.state
+    console.log(city)
+    console.log(state)
+    $('#yourLocation').text('Yo Location: ' + city + ' , ' + state)
     zip = data.location.zip
     console.log(zip)
     var forecastUrl = apiUrl + 'forecast/q/' +zip+ '.json'
@@ -25,12 +31,17 @@ function init() {
   })
 }
 var zip;
-
+var locData;
 var defaultData;
 var areaData;
 function getWeather(){
   zip = $("#zip").val();
   var forecastUrl = apiUrl + 'forecast/q/' + zip+ '.json';
+  geoUrl = apiUrl+ '/geolookup/q/' + zip+ '.json'
+  $.get(geoUrl)
+  .done(function(data){
+    locData = data
+  
   $.get(forecastUrl)
   .done(function(data){
     areaData = data;
@@ -41,6 +52,7 @@ function getWeather(){
     console.error(error);
   });
 
+})
 }
 
 function fiveDay(){
@@ -51,6 +63,12 @@ function fiveDay(){
     var img = $('<img/>', {
       src: currIcon
     });
+
+    var city = locData.location.city
+    var state = locData.location.state
+    console.log(city)
+    console.log(state)
+    $('#yourLocation').text('Yo Location: ' + city + ' , ' + state)
     $('.day' + x + ' .icon').empty();
     var currText = currForecast.fcttext
     var currDat = currForecast.title
